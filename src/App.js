a2d892063c7b10df4bdd2d4e3bf2043506ef5d09
Adam Bretzer <depthhoar@Adams-MacBook-Pro.local>
2020-10-31 18:23:57 -0600

    make it better

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import 'tachyons';
+import Tilt from 'react-tilt';
+import 'bootstrap';
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Tilt className="Tilt br2 shadow-2" options={{ max : 25 }} style={{ height: 250, width: 250 }} >
+        <div className="Tilt-inner"> <img style={{paddingTop: '5px'}} src="/images/Screen Shot 2020-10-31 at 2.33.55 PM.png" alt=""/> </div>
+      </Tilt>
+      
+      <div>
+        <center>
+          <img  style={{width: 400, height: 400, borderRadius: 400}} src="/images/IMG_5767.jpg" class="mw-50" alt="headshot"/>
+         
+        </center>
+      </div>
+
+        <section id="about">
+          <div class="container">
+            <div class="row">
+              <div class="col-4 my-col">
+                row 1 col 1
+              </div>
+            </div>
+          </div>
+        </section>
+
+
     </div>
+    
+    
   );
 }
 
